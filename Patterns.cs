8ed37e47d655912fd23a6fd1672b030a89bd8d3e
Viney <vineykhaneja999@gmail.com>
2022-02-28 15:22:07 +0530

    Add project files.

---
+++ Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Problem_Statements
+{
+    internal class Patterns
+    {
+        public static string RegexPinCode = "^[1-9][0-9]{5}$";
+        public void ValidatePinCode(string pinCode)
+        {
+           string result = Regex.IsMatch(pinCode, RegexPinCode)? "PinCode entered is valid" : "PinCode entered is not valid";
+           Console.WriteLine(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Regex_Problem_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            Console.Write("Enter the pin code : ");
+            string pinCode = Console.ReadLine();
+            patterns.ValidatePinCode(pinCode);  
+        }
+    }
+}
